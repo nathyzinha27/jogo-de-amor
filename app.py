1025a1eadd300a3c4ece511ab10807c12a9a46d8
nathyzinha27 <nathalinha77@gmail.com>
2026-01-29 18:54:36 -0300

    corrige erro 500 com session

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from data import fases
+from flask import Flask, session, request, redirect, url_for, render_template
 
 app = Flask(__name__)
-app.secret_key = "amor_secreto"
+app.secret_key = "chave-secreta-super-romantica"
+
+fases = [
+    "Pergunta 1",
+    "Pergunta 2",
+    "Pergunta 3"
+]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     if "fase" not in session:
         session["fase"] = 0
 
-    fase_atual = session["fase"]
-
     if request.method == "POST":
         session["fase"] += 1
+
         if session["fase"] >= len(fases):
             return redirect(url_for("pedido"))
+
         return redirect(url_for("index"))
 
+    fase_atual = session["fase"]
+
     return render_template("index.html", fase=fases[fase_atual])
+
 
 @app.route("/pedido")
 def pedido():
     return render_template("pedido.html")
+
 
 @app.route("/resposta", methods=["POST"])
 def resposta():
@@ -30,6 +40,6 @@ def resposta():
         return render_template("sim.html")
     return render_template("nao.html")
 
+
 if __name__ == "__main__":
     app.run()
-
